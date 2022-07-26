38210246e630948a1775e1ee95ae37153f1013e8
jrildosilva <jrsrildo44@gmail.com>
2022-07-26 18:48:17 -0300

    criação das categorieas e repositorie

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,17 @@
 import {  Router } from 'express';
-import {  v4 as uuidV4 } from 'uuid'; 
 
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-
-    const category = {
-        name,
-        description,
-        id: uuidV4()
-    }
-
-    categories.push(category); 
-
+    
+    categoriesRepository.create({ name, description });
+    
     return response.status(201).send();
-})
+});
 
 export { categoriesRoutes };
