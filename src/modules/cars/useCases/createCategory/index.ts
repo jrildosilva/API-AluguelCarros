b6253a698140d2345a6f8efe16c8f085e229f80e
jrildosilva <jrsrildo44@gmail.com>
2022-08-02 19:58:55 -0300

    refatorando listagem de categoria

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,12 +1,16 @@
 import { CategoriesRepository } from "../../repositories/CategoriesRepository";
-import { CreateCategoryController } from "./createcategoryController";
+import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 
 
 const categoriesRepository = new CategoriesRepository();
-const createcategoryUsecase = new CreateCategoryUseCase(categoriesRepository);
-const createCategoryController = new CreateCategoryController( createcategoryUsecase);
+
+const createcategoryUsecase = new CreateCategoryUseCase
+(categoriesRepository);
+
+const createCategoryController = new CreateCategoryController( 
+    createcategoryUsecase);
 
 
 export { createCategoryController };
